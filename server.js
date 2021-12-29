a39cd7c25d83389d4eedc1b66f1f9f1056dd18af
Kundanpatel456 <88932550+Kundanpatel456@users.noreply.github.com>
2021-12-29 23:17:15 +0530

    Update server.js

--- server.js
+++ server.js
@@ -165,14 +165,14 @@ app.post('/login-user', (req, res) => {
               console.log("valid");
               // window.navigate("Noice")
               // res.redirect('/Noicedata');
-              openwin("http://" + req.headers['host'] + '/NoiceData.html');
-              console.log("http://" + req.headers['host'] + '/NoiceData.html');
+              openwin("http://www." + req.headers['host'] + '/NoiceData.html');
+              console.log("http://www." + req.headers['host'] + '/NoiceData.html');
               return res.redirect('/NoiceData');
 
 
               // window.replace("http://" + req.headers['host'] + '/NoiceData.html')
 
-              // res.writeHead(301, { "Location": "http://www." + req.headers['host'] + '/NoiceData.html' });
+              // res.writeHead(301, { "Location": "http://" + req.headers['host'] + '/NoiceData.html' });
               // return res.end()
 
               // res.json("CredentialsAreValid")
