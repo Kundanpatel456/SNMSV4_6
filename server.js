05a92168afa665b5157098abf64f0db253437918
Kundanpatel456 <88932550+Kundanpatel456@users.noreply.github.com>
2021-12-29 23:56:10 +0530

    Update server.js

--- server.js
+++ server.js
@@ -165,8 +165,8 @@ app.post('/login-user', (req, res) => {
               console.log("valid");
               // window.navigate("Noice")
               // res.redirect('/Noicedata');
-              openwin("http://" + req.headers['host'] + '/NoiceData.html');
-              console.log("http://" + req.headers['host'] + '/NoiceData.html');
+              openwin("http://" + req.headers['host'] + '/NoiceData.html',"_self");
+              console.log("http://" + req.headers['host'] + '/NoiceData.html',"_self");
               return res.redirect('/NoiceData');
 
 
